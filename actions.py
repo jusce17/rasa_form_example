00d341120ef393a0c6de66d6161e5358f82286a0
Eden <eden.juscelino@gmail.com>
2020-09-23 21:27:41 +0100

    add custom actions

--- actions.py
+++ actions.py
@@ -7,21 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, ConversationPaused, ConversationResumed
+from rasa_sdk.forms import FormAction
+import logging
+from rasa_sdk.events import ReminderScheduled
+
+
+        return []
+
+
+class ActionUserMessage(FormAction):
+
+    def name(self) -> Text:
+        return "form_get_client_message"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill."""
+
+        return ["client_name", "client_email", "client_message"]
+
+    def submit(self,dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any],):
+
+        # Do something dramatically here, like call an API for example lol
+        dispatcher.utter_message(text='TEST')
+
+        return []
